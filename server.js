b53ae4754047da77469a016496ebf83280e9e22b
Maheswari <dmahi3224@gmail.com>
2026-05-18 10:47:38 +0530

    updated code

--- server.js
+++ server.js
@@ -1,24 +1,30 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors'); // add this
 require('dotenv').config();
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('CORS Middleware:', req.method, req.url);
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-auth-token');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  if (req.method === 'OPTIONS') return res.sendStatus(200);
-  next();
-});
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://studyplanner-client.vercel.app'
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
 
 app.use(express.json());
 app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api/exams', require('./routes/examRoutes')); 
+app.use('/api/exams', require('./routes/examRoutes'));
 app.use('/api/schedule', require('./routes/scheduleRoutes'));
 
-
 mongoose.connect(process.env.MONGO_URI).then(() => console.log('MongoDB connected'));
-app.listen(5000, () => console.log('Server running on port 5000'));
+app.listen(process.env.PORT || 5000, () => console.log('Server running'));
