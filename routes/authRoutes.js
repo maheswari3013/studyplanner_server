7216c14b4f56671f9f44054576c86c73b11e1386
Maheswari <dmahi3224@gmail.com>
2026-05-18 19:08:33 +0530

    fix code

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,16 +10,13 @@ const auth = require('../middleware/auth');
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true, // true for port 465
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   },
-  family: 4, 
-  tls: {
-    rejectUnauthorized: false
-  }
+  family: 4
 })
 
 // POST /api/auth/send-otp
