e2dc66d168eb953e61f6ce85232cbba54e62c8d1
Maheswari <dmahi3224@gmail.com>
2026-05-19 12:47:08 +0530

    fix: enire code

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/User'); // You need this import
 
-module.exports = function(req, res, next) {
+module.exports = async function(req, res, next) { // ← add async here
   const token = req.header('x-auth-token');
 
   if (!token) {
@@ -10,9 +11,11 @@ module.exports = function(req, res, next) {
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = { _id: decoded.id };
+    
+    // Move the await INSIDE the function
+    await User.findByIdAndUpdate(decoded.id, { lastActive: new Date() }); // ← fixed
     next();
   } catch (err) {
     res.status(401).json({ msg: 'Token is not valid' });
   }
 };
-await User.findByIdAndUpdate(decoded.user.id, { lastActive: new Date() });
